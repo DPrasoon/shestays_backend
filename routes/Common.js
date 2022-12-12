a427eb128f883168cb8e91bb73cc6a8b8a6329b6
Prasoon Dubey <prasoonid@gmail.com>
2022-12-12 20:46:06 +0530

    minor updates

--- routes/Common.js
+++ routes/Common.js
@@ -4,11 +4,6 @@ const User = require('../models/User')
 const Feedback = require('../models/Feedback')
 const Query = require('../models/Query')
 
-// Get All Accomodations route (Retrieve)
-// router.get('/', async (req, res) => {
-//     const accomodation = await Accomodation.find();
-//     res.json(accomodation);
-// })
 
 // Create new user  
 router.post('/user_registration', async (req, res) => {
@@ -61,32 +56,6 @@ router.post('/contact', async (req, res) => {
     const savedQuery = await newQuery.save() // mongo save method
     res.json(savedQuery) // respond with json to our post endpoint
 });
-
-// Getter by id
-// router.get('/get/:id', async (req, res) => {
-//     const t = await Todo.findById({ _id: req.params.id })
-//     res.json(t)
-// })
-
-// Delete a todo by id
-// router.delete('/delete/:id', async (req, res) => {
-//     const tDelete = await Todo.findByIdAndDelete({ _id: req.params.id })
-//     res.json(tDelete)
-// })
-
-// Update a todo by id
-// router.put('/update/:id', async (req, res) => {
-//     const tUpdate = await Todo.updateOne(
-//         { _id: req.params.id },
-
-//         // { $set: req.body }
-//         {
-//            author: "UpdateEngine",
-//            todo: "Coding"
-//          }
-//     )
-//     res.json(tUpdate)
-// })
 
 
 //exporting router so it can be used anywhere
